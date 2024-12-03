a9b7feb69d6569db412e29443799681cc3de2794
Igor Motyka <igor_motyka@mail.com>
2024-12-03 22:25:38 +0100

    Day 2 addendum

--- day2.rs
+++ day2.rs
@@ -1,8 +1,18 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+fn prepate_permutations(arr: &Vec<isize>) -> Vec<Vec<isize>> {
+    let mut permutations: Vec<Vec<isize>> = vec![];
+    for i in 0..arr.len() {
+        let mut variant = arr.clone();
+        variant.remove(i);
+        permutations.push(variant);
+    }
+    permutations 
+}
+
 fn main() {
-    let file = File::open("day2_t.txt").expect("Failed to read day2.txt file");
+    let file = File::open("day2.txt").expect("Failed to read day2.txt file");
     let buffer = BufReader::new(file);
 
     let mut good_seq = 0;
@@ -10,19 +20,20 @@ fn main() {
         match line {
             Ok(line_str) => {
                 let split: Vec<isize> = line_str.split(' ').map(|x| str::parse(x).unwrap()).collect();
-                let diffs: Vec<_>     = split.windows(2).map(|pair| pair[0] - pair[1]).collect();
 
-                let increasing = diffs[0] > 0;
+                let permutations = prepate_permutations(&split);
 
-                let tmp: Vec<_> = diffs.clone().into_iter()
-                    .map(|v| if (increasing && v > 0 && v <= 3) || (!increasing && v < 0 && v >= -3) { true } else { false })
-                    .collect();
+                for arr in permutations {
+                    let diffs: Vec<_> = arr.windows(2).map(|pair| pair[0] - pair[1]).collect();
+                    let increasing = diffs[0] > 0;
 
-                let is_good_seq = tmp.clone().into_iter().all(|v| v);
-                println!("{:?} {:?} {:?} {:?}", line_str, diffs, tmp, is_good_seq);
-
-                if is_good_seq {
-                    good_seq+=1;
+                    let is_good_seq = diffs.into_iter()
+                        .map(|v| if (increasing && v > 0 && v <= 3) || (!increasing && v < 0 && v >= -3) { true } else { false })
+                        .all(|v| v);
+                    if is_good_seq {
+                        good_seq+=1;
+                        break;
+                    }
                 }
 
             },
